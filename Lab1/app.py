6f6874ca9d4f00fc64b92fccc389c4f4fca6999d
Dima Vasilenko <dimavasilenkoad@mail.ru>
2024-04-21 23:14:36 +0300

    added a time input field

--- Lab1/app.py
+++ Lab1/app.py
@@ -2,7 +2,7 @@ import sys
 
 import numpy as np
 from PyQt6 import QtWidgets
-from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton, QVBoxLayout
+from PyQt6.QtWidgets import QApplication, QMainWindow, QFileDialog, QPushButton, QVBoxLayout, QLineEdit, QLabel
 from PyQt6.QtGui import QAction
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
@@ -17,7 +17,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Read data")
+        self.setWindowTitle("Equation solution")
         self.setGeometry(100, 100, 500, 400)
 
         self.model_filename = None
@@ -33,11 +33,23 @@ class MainWindow(QMainWindow):
         self.plot_button = QPushButton("Draw plot")
         self.plot_button.clicked.connect(self.draw_plot)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.plot_button)
+        self.time_input_label = QLabel("Enter a time:")
+
+        self.time_input_field = QLineEdit()
+
+        self.submit_button = QPushButton('Submit')
+        self.submit_button.clicked.connect(self.time_input)
+
         self.figure = Figure()
         self.canvas = FigureCanvas(self.figure)
+
+        layout = QVBoxLayout()
+
+        layout.addWidget(self.time_input_label)
+        layout.addWidget(self.time_input_field)
+        layout.addWidget(self.submit_button)
         layout.addWidget(self.canvas)
+        layout.addWidget(self.plot_button)
 
 
         widget = QtWidgets.QWidget()
@@ -64,6 +76,15 @@ class MainWindow(QMainWindow):
         action = QAction("Save results", self)
         action.triggered.connect(self.save_results)
         toolbar.addAction(action)
+
+    def time_input(self):
+        self.figure.clear()
+        time_str = self.time_input_field.text()
+        try:
+            time = int(time_str)
+            self.time = time
+        except ValueError:
+            print("Error: Unable to convert text into int")
 
 
     def choose_coefficients(self):
@@ -101,17 +122,24 @@ class MainWindow(QMainWindow):
             self.directory_path = directory_name
 
     def draw_plot(self):
-        self.time = 100
-        self.points = 1001
+        points = self.time * 10 + 1
 
         solver = EquationSolver(self.mesh, self.coefficients, self.initial_values)
-        time, ode_sol = solver.solve_equatuon(self.time, self.points)
+        time, ode_sol = solver.solve_equatuon(self.time, points)
 
         np.savetxt(self.directory_path + '/' + 'results.csv', np.column_stack([time, ode_sol]),
                    delimiter=",", fmt='%10.5f')
 
         ax = self.figure.add_subplot(111)
-        ax.plot(time, ode_sol)
+        self.figure.xla
+
+        ax.plot(time, ode_sol[:, 0], color='brown', label="T_1(t)")
+        ax.plot(time, ode_sol[:, 1], color='red', label="T_2(t)")
+        ax.plot(time, ode_sol[:, 2], color='orange', label="T_3(t)")
+        ax.plot(time, ode_sol[:, 3], color='green', label="T_4(t)")
+        ax.plot(time, ode_sol[:, 4], color='blue', label="T_5(t)")
+        ax.legend()
+
         self.canvas.draw()
 
 
